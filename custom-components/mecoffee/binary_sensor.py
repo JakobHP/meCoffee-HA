0259bb032b0652533e9b450b64db994fa7fb2432
JakobHP <jakob@poulsen.nu>
2026-03-09 18:10:38 +0100

    Delete custom-components/mecoffee directory

--- custom-components/mecoffee/binary_sensor.py
+++
@@ -1,9 +0,0 @@
-from homeassistant.components.binary_sensor import BinarySensorEntity
-
-
-class HeaterBinarySensor(BinarySensorEntity):
-
-    @property
-    def is_on(self):
-
-        return self.coordinator.data.get("heater")
